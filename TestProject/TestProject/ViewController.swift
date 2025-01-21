28e0d1e20120a72185cfdb3e6f0fb1ae62e8f436
Dmitry Zabiyakin <zabiyakin09@yandex.ru>
2025-01-21 17:32:06 +0300

    Start Commit

---
+++ TestProject/TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Дмитрий Забиякин on 21.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
